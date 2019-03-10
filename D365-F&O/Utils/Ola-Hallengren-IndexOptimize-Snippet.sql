7a8d72aeaa826ee7738be85ac12f341173151a95
Marcel Domingus <mrjdomingus@users.noreply.github.com>
2019-03-10 01:27:50 -0800

    Update Ola-Hallengren-IndexOptimize-Snippet.sql

--- D365-F&O/Utils/Ola-Hallengren-IndexOptimize-Snippet.sql
+++ D365-F&O/Utils/Ola-Hallengren-IndexOptimize-Snippet.sql
@@ -1,4 +1,4 @@
-EXECUTE dbo.IndexOptimize @Databases = 'AX_Live,AX_Live_model',
+EXECUTE dbo.IndexOptimize @Databases = 'AxDB,DYNAMICSXREFDB',
 @FragmentationLow = NULL,
 @FragmentationMedium = 'INDEX_REORGANIZE,INDEX_REBUILD_ONLINE,INDEX_REBUILD_OFFLINE',
 @FragmentationHigh = 'INDEX_REBUILD_ONLINE,INDEX_REBUILD_OFFLINE',
