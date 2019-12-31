5b2d75a568bdb2f20ea4b73f7b1cd1c6a1238d19
Marcel Domingus <domingus@xs4all.nl>
2019-12-31 07:46:53 +0100

    2nd attempt to add Nuxt-TS-Example

---
+++ Javascript/Nuxt-TS-Example/README.md
@@ -0,0 +1,25 @@
+# nuxt-ts
+
+> My bee&#39;s knees Nuxt.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ npm run install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm run start
+
+# generate static project
+$ npm run generate
+```
+
+For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
+
+Also see:
+* [Vue Composition API](https://vue-composition-api-rfc.netlify.com/#summary)
---
+++ Javascript/Nuxt-TS-Example/pages/composition_api.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-layout>
+    <v-flex class="text-center">
+      <img src="/v.png" alt="Vuetify.js" class="mb-5">
+      <blockquote class="blockquote">
+        &#8220;{{ message }}&#8221;
+        <footer>
+          <small>
+            <em>&mdash;John Johnson</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script lang='ts'>
+import { createComponent, ref } from '@vue/composition-api'
+
+export default createComponent({
+  setup () {
+    const message = ref('First, solve the problem. Then, write the code using the Composition API.')
+
+    return {
+      message
+    }
+  }
+})
+</script>
